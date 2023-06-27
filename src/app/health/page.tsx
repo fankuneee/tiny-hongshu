740db3803cf3a8edf73677511704d97248ed873e
huzhenghui <huzhenghui@miluoinfo.com>
2023-06-27 15:44:32 +0800

    init

---
+++ src/app/health/page.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Author: huzhenghui
+ * @Date: 2023-06-27 09:55:48
+ * @LastEditors: huzhenghui
+ * @LastEditTime: 2023-06-27 13:18:10
+ */
+
+
+const MyPage: React.FC = () => {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      
+    </main>
+  )
+}
+
+export default MyPage;
---
+++ src/app/travel/details/page.tsx
@@ -0,0 +1,154 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Author: huzhenghui
+ * @Date: 2023-06-27 13:21:25
+ * @LastEditors: huzhenghui
+ * @LastEditTime: 2023-06-27 14:29:48
+ */
+const TravelDetailsPage: React.FC = () => {
+  const arr = new Array(5).fill(0)
+  return (
+    <main className="flex flex-col justify-between px-36 py-6 overflow-y-auto h-4/5 ">
+      <div className="flex flex-row items-center h-16">
+        <div className="text-4xl">07:00</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1 bg-white mx-10"></div>
+        <div className="bg-cyan-300/10 rounded-lg shadow-xl flex-grow p-2">
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center h-16">
+        <div className="text-4xl">07:00</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="border-l border-dashed border-gray-500 mx-10"></div>
+        <div className="bg-cyan-300/10 rounded-lg shadow-xl flex-grow p-2">
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center h-16">
+        <div className="text-4xl">07:00</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1 bg-white mx-10"></div>
+        <div className="bg-cyan-300/10 rounded-lg shadow-xl flex-grow p-2">
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center h-16">
+        <div className="text-4xl">07:00</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1 bg-white mx-10"></div>
+        <div className="bg-cyan-300/10 rounded-lg shadow-xl flex-grow p-2">
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center h-16">
+        <div className="text-4xl">07:00</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1 bg-white mx-10"></div>
+        <div className="bg-cyan-300/10 rounded-lg shadow-xl flex-grow p-2">
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center h-16">
+        <div className="text-4xl">07:00</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1 bg-white mx-10"></div>
+        <div className="bg-cyan-300/10 rounded-lg shadow-xl flex-grow p-2">
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center h-16">
+        <div className="text-4xl">07:00</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1 bg-white mx-10"></div>
+        <div className="bg-cyan-300/10 rounded-lg shadow-xl flex-grow p-2">
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center h-16">
+        <div className="text-4xl">07:00</div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1 bg-white mx-10"></div>
+        <div className="bg-cyan-300/10 rounded-lg shadow-xl flex-grow p-2">
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+          <p>asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdas</p>
+        </div>
+      </div>
+      
+    </main>
+  )
+}
+
+export default TravelDetailsPage;
